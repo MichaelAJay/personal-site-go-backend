4ec96da97626233708f9e54c00101b060939a3ed
mjay <michael@merged.group>
2024-01-16 21:20:55 -0500

    added db client

---
+++ pkg/services/db-client.go
@@ -0,0 +1,28 @@
+package db_client
+
+import (
+	"sync"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+var once sync.Once
+
+func db_client() *gorm.DB {
+	once.Do(func() {
+		dsn := "TODO"
+		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+			SkipDefaultTransaction: true,
+		})
+
+		if err != nil {
+			panic("failed to connect database")
+		}
+
+		// Migrate the schema
+		db.AutoMigrate()
+	})
+	return db
+}
